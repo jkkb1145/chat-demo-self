036403a0ea80c29fb7e9979221f2883270ba4d6c
jkkb <jkkb114514@gmail.com>
2026-03-22 18:52:38 +0800

    完成配置文件初始化

---
+++ conf/config.go
@@ -0,0 +1,68 @@
+package conf
+
+import (
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"gopkg.in/ini.v1"
+)
+
+var (
+	AppMode  string
+	HttpPort string
+
+	Db         string
+	DbHost     string
+	DbPort     string
+	DbUser     string
+	DbPassWord string
+	DbName     string
+
+	RedisDb       string
+	RedisAddr     string
+	RedisPassWord string
+	RedisDbName   string
+
+	MongoDBClient   *mongo.Client
+	MongoDBName     string
+	MongoDBAddr     string
+	MongoDBPassWord string
+	MongoDBPort     string
+)
+
+func Init() {
+	file, err := ini.Load("D:\\GoDemo\\chat-demo-self\\conf\\config.ini")
+	if err != nil {
+		panic(err)
+	}
+	LoadServer(file)
+	LoadMySQL(file)
+	LoadRedis(file)
+	LoadMongoDB(file)
+}
+
+func LoadServer(file *ini.File) {
+	AppMode = file.Section("service").Key("AppMode").String()
+	HttpPort = file.Section("service").Key("HttpPort").String()
+}
+
+func LoadMySQL(file *ini.File) {
+	Db = file.Section("mysql").Key("Db").String()
+	DbHost = file.Section("mysql").Key("DbHost").String()
+	DbPort = file.Section("mysql").Key("DbPort").String()
+	DbUser = file.Section("mysql").Key("DbUser").String()
+	DbPassWord = file.Section("mysql").Key("DbPassWord").String()
+	DbName = file.Section("mysql").Key("DbName").String()
+}
+
+func LoadRedis(file *ini.File) {
+	RedisDb = file.Section("redis").Key("RedisDb").String()
+	RedisAddr = file.Section("redis").Key("RedisAddr").String()
+	RedisPassWord = file.Section("redis").Key("RedisPassWord").String()
+	RedisDbName = file.Section("redis").Key("RedisDbName").String()
+}
+
+func LoadMongoDB(file *ini.File) {
+	MongoDBName = file.Section("mongodb").Key("MongoDBName").String()
+	MongoDBAddr = file.Section("mongodb").Key("MongoDBAddr").String()
+	MongoDBPassWord = file.Section("mongodb").Key("MongoDBPassWord").String()
+	MongoDBPort = file.Section("mongodb").Key("MongoDBPort").String()
+}
